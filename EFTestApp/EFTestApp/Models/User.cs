0b7721d1d45d438aa1363256996c5ec22825424c
keki228 <nursultan.aldaiarov@gmail.com>
2020-08-26 16:09:26 +0600

    Released (for testing) small app that connects to sql db to make basic CRUD operations.

---
+++ EFTestApp/EFTestApp/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFTestApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}\nName: {Name}\nAge: {Age}";
+        }
+    }
+}
---
+++ EFTestApp/EFTestApp/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Markup;
+using EFTestApp.Models;
+using AppContext = EFTestApp.Models.AppContext;
+
+namespace EFTestApp
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            string choice = "";
+            while (choice != "0")
+            {
+                Console.WriteLine("\n0 - to exit" +
+                    "\n1 - to create new user" +
+                    "\n2 - to read list of users" +
+                    "\n3 - to edit existing user" +
+                    "\n4 - to remove user");
+                choice = Console.ReadLine();
+                if (!int.TryParse(choice, out int ch))
+                {
+                    continue;
+                }
+                switch (ch)
+                {
+                    case 1:
+                        AddUser();
+                        break;
+                    case 2:
+                        ShowUsers();
+                        break;
+                    case 3:
+                        EditUser();
+                        break;
+                    case 4:
+                        RemoveUser();
+                        break;
+                    default:
+                        break;
+                }
+            }           
+        }
+        public static void AddUser()
+        {
+            using (AppContext db = new AppContext())
+            {
+                Console.WriteLine("Enter name: ");
+                string name = Console.ReadLine();
+                string temp;
+                int age;
+                do
+                {
+                    Console.WriteLine("Enter age: ");
+                    temp = Console.ReadLine();
+                }
+                while (!int.TryParse(temp, out age));
+                var user = new User { Name = name, Age = age };
+                db.Users.Add(user);
+                db.SaveChanges();
+                Console.WriteLine("User has been successfully saved.");
+            }
+        }
+        public static void ShowUsers()
+        {
+            using (AppContext db = new AppContext())
+            {
+                var users = db.Users.ToList();
+                foreach(var user in users)
+                {
+                    Console.WriteLine($"{user.Id} - {user.Name} - {user.Age}");
+                }
+            }
+        }
+        public static void EditUser()
+        {
+            using (AppContext db = new AppContext())
+            {
+                User user = null;
+                Console.WriteLine("Enter name: ");
+                string name = Console.ReadLine();
+                user = db.Users.FirstOrDefault(x => x.Name == name);
+                if(user!=null)
+                {
+                    Console.WriteLine("Enter new name: ");
+                    name = Console.ReadLine();
+                    user.Name = name;
+
+                    string temp;
+                    int age;
+                    do
+                    {
+                        Console.WriteLine("Enter age: ");
+                        temp = Console.ReadLine();
+                    }
+                    while (!int.TryParse(temp, out age));
+                    user.Age = age;
+                    db.Users.Update(user);
+                    db.SaveChanges();
+                    Console.WriteLine("User was updated.");
+                }
+                else
+                {
+                    Console.WriteLine("User does not exist.");
+                }
+            }
+        }
+        public static void RemoveUser()
+        {
+            using(AppContext db = new AppContext())
+            {
+                User user = null;
+                Console.WriteLine("Enter name: ");
+                string name = Console.ReadLine();
+                user = db.Users.FirstOrDefault(x => x.Name == name);
+                if(user!=null)
+                {
+                    db.Users.Remove(user);
+                    db.SaveChanges();
+                    Console.WriteLine("User has been successfully removed.");
+                }
+                else
+                {
+                    Console.WriteLine("User was not found.");
+                }
+            }
+        }
+    }
+}
