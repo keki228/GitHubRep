467982303a133c3a5f33413b4111e0cbce9406cd
keki228 <nursultan.aldaiarov@gmail.com>
2020-08-28 16:35:10 +0600

    Relaesed eager and exlicit loading.

--- EFTestApp/EFTestApp/Program.cs
+++ EFTestApp/EFTestApp/Program.cs
@@ -13,38 +13,66 @@ namespace EFTestApp
         {
             using (AppContext db = new AppContext())
             {
-                Company ts = new Company { Name = "TimelySoft" };
-                Company google = new Company { Name = "Google" };
+                Position manager = new Position { Name = "Manager" };
+                Position basic = new Position { Name = "Basic" };
+                Position developer = new Position { Name = "Developer" };
+                db.Positions.AddRange(manager, basic, developer);
+
+                City bish = new City { Name = "Bishkek" };
+                City osh = new City { Name = "Osh" };
+                City moscow = new City { Name = "Moscow" };
+                db.Cities.AddRange(bish, osh, moscow);
+
+                Country rus = new Country { Name = "Russia", Capital = moscow };
+                Country kyr = new Country { Name = "Kyrgyzstan", Capital = bish };
+                db.AddRange(rus, kyr);
+
+                Company ts = new Company { Name = "TimelySoft", Country = kyr };
+                Company google = new Company { Name = "Google", Country = rus };
                 db.Companies.AddRange(ts, google);
                 db.SaveChanges();
 
-                User nurs = new User { Name = "Nursultan", Company = ts };
-                User aman = new User { Name = "Amangeldi", Company = ts };
-                User sezim = new User { Name = "Sezim", Company = google };
+                User nurs = new User { Name = "Nursultan", Company = ts, Position = basic };
+                User aman = new User { Name = "Amangeldi", Company = ts, Position = developer };
+                User sezim = new User { Name = "Sezim", Company = google, Position = manager };
                 User unknown = new User { Name = "X3", Company = google };
                 User useless = new User { Name = "lazyman" };
                 db.Users.AddRange(nurs, aman, sezim, unknown, useless);
                 db.SaveChanges();
 
+                //Eager Loading
                 var users = db.Users
                     .Include(u => u.Company)
-                    .ToList();
+                        .ThenInclude(comp => comp.Country)
+                            .ThenInclude(country => country.Capital)
+                    .Include(u => u.Position);
                 foreach(var user in users)
                 {
-                    Console.WriteLine($"{user.Name} - {user.Company?.Name}");
+                    Console.WriteLine($"{user.Name} - {user.Position?.Name}");
+                    Console.WriteLine($"{user.Company?.Name} - {user.Company?.Country.Name} - {user.Company?.Country.Capital.Name}");
+                    Console.WriteLine("--------------------------------");
                 }
 
-                var companies = db.Companies
-                    .Include(u => u.Users)
-                    .ToList();
-                foreach(var company in companies)
+                //Explicit Loading
+                Company company = db.Companies.FirstOrDefault();
+                db.Users.Where(u => u.CompanyId == company.Id).Load();
+                Console.WriteLine($"Company: {company.Name}");
+                foreach(var user in company.Users)
                 {
-                    Console.WriteLine($"Company: {company.Name}");
-                    foreach(var user in company.Users)
-                    {
-                        Console.WriteLine(user.Name);
-                    }
+                    Console.WriteLine($"{user.Name} - {user.Position}");
                 }
+
+                Company company2 = db.Companies.FirstOrDefault();
+                db.Entry(company2).Collection(t => t.Users).Load();
+                Console.WriteLine($"Company2: {company2.Name}");
+                foreach (var user in company2.Users)
+                {
+                    Console.WriteLine($"{user.Name} - {user.Position}");
+                }
+
+                User uservar = db.Users.FirstOrDefault(x => x.Company!=null);
+                db.Entry(uservar).Reference(x => x.Company).Load();
+                Console.WriteLine($"{uservar.Name} - {uservar.Company?.Name}");
             }
         }
     }
